b5f35771a480210333e01f414e2831ca505b2938
刘佳丽 <liujiali3@jd.com>
2021-11-24 17:42:17 +0800

    今日不宜打代码

--- exercise/cpp/0_code743.cpp
+++ exercise/cpp/0_code743.cpp
@@ -3,67 +3,60 @@
 class Solution
 {
 public:
+    bool cmp(const pair<int, int> &a, const pair<int, int> &b)
+    {
+        return a.second < b.second;
+    }
     int networkDelayTime(vector<vector<int>> &times, int n, int k)
     {
-        vector<vector<int>> vec(n, vector<int>(n, INT_MAX));
-        int first_index[2];
-        int value = INT_MAX;
+        map<string, int> side;
         for (int i = 0; i < times.size(); i++)
         {
-            vec[times[i][0] - 1][times[i][1] - 1] = times[i][2];
-            if (times[i][2] < value)
-            {
-                first_index[0] = times[i][0] - 1;
-                first_index[1] = times[i][1] - 1;
-            }
+            string temp = to_string(times[i][0] - 1) + "_" + to_string(times[i][1] - 1);
+            side.insert({temp, times[i][2]});
         }
+        vector<pair<int, int>> _map;
         for (int i = 0; i < n; i++)
         {
-            vec[i][i] = 0;
-        }
-        vector<int> flag = {k - 1};
-
-        for (int i = 0; i < n - 1; i++)
-        {
-            int index;
-            int lens = INT_MAX;
-            for (int j = 0; j < flag.size(); j++)
+            if (i != k - 1)
             {
-                for (int kk = 0; kk < n; kk++)
-                {
-                    if (vec[flag[j]][kk] < lens && find(flag.begin(), flag.end(), kk) == flag.end())
-                    {
-                        index = kk;
-                        lens = vec[flag[j]][kk];
-                    }
-                }
-            }
-            if (lens == INT_MAX)
-            {
-                return -1;
-            }
-            else
-            {
-                flag.push_back(index);
-            }
-
-            for (int j = 0; j < n; j++)
-            {
-                if (vec[k - 1][index] != INT_MAX && vec[index][j] != INT_MAX && vec[k - 1][j] > vec[k - 1][index] + vec[index][j])
-                {
-                    vec[k - 1][j] = vec[k - 1][index] + vec[index][j];
-                }
+                map<string, int>::iterator it = side.find(to_string(k - 1) + "_" + to_string(i));
+                if (it == side.end())
+                    _map.push_back(make_pair(i, INT_MAX));
+                else
+                    _map.push_back(make_pair(i, it->second));
             }
         }
-        int maxPosition = max_element(vec[k - 1].begin(), vec[k - 1].end()) - vec[k - 1].begin();
-        return vec[k - 1][maxPosition];
+
+        // for (vector<pair<int, int>>::iterator i = _map.begin(); i != _map.end(); i++)
+        // {
+        //     auto item = min_element(_map.begin(), _map.end());
+        //     if (item->second != INT_MAX)
+        //         _map.erase(item->first);
+        //     else
+        //         return -1;
+
+        //     for (auto mm : _map)
+        //     {
+        //         map<string, int>::iterator it = side.find(to_string(item->first) + "_" + to_string(mm.first));
+        //         if (it == side.end())
+        //             continue;
+
+        //         if (mm.second > item->second + it->second)
+        //         {
+        //             mm.second = item->second + it->second;
+        //         }
+        //     }
+        // }
+        stable_sort(_map.begin(), _map.end(), cmp);
+        return 1;
     }
 };
 
 int main()
 {
     Solution S;
-    vector<vector<int>> aa = {{2, 1, 1}, {2, 3, 1}, {3, 4, 1}};
+    vector<vector<int>> aa = {{2, 1, 4}, {2, 3, 3}, {3, 4, 5}};
     int n = 4, k = 2;
     cout << S.networkDelayTime(aa, n, k) << endl;
 }
--- exercise/cpp/README.md
+++ exercise/cpp/README.md
@@ -28,3 +28,75 @@ sort(matchsticks.begin(), matchsticks.end(), greater<int>());
 //从小到大
 sort(matchsticks.begin(), matchsticks.end(), less<int>());
 ```
+
+### map插入数据
+[参考](https://blog.csdn.net/Leo_csdn_/article/details/86589904)
+```
+    map<string, int> m2;
+    m2.insert({ "abc", 1 });                    //----1
+    m2.insert(make_pair(string("def"), 2));     //----2
+    m2.insert(pair<string, int>(string("ghi"), 3));     //----3
+```
+
+### map delete
+```
+//删除键为k1指向的元素
+cmap.erase("k1");
+//删除迭代器 key所指向的元素
+map<string,int>::iterator key = cmap.find("mykey");
+  if(key!=cmap.end())
+ {
+	cmap.erase(key);
+ }
+//删除所有元素
+cmap.erase(cmap.begin(),cmap.end());
+```
+[参考](https://blog.csdn.net/zvall/article/details/52267007)
+
+### map find
+```
+map<int, string>::iterator iter = mapStudent.find(1);
+if(iter != mapStudent.end())
+    Cout<<”Find, the value is ”<<iter->second<<endl;
+else
+    Cout<<”Do not Find”<<endl;
+```
+
+### map的元素个数
+```
+_map.size()
+```
+
+### map最大值最小值
+
+
+### vector二维求一维长度，二维长度
+```
+//一维
+vec[0].size()
+//二维
+vec.size()
+```
+
+### int转string
+[参考](https://blog.csdn.net/chavo0/article/details/51038397)
+```
+to_string()
+```
+
+### 数组最大值/最小值
+1. 求数组的最大值或最小值
+* vector容器
+最大值：int maxValue = *max_element(v.begin(),v.end()); 
+最小值：int minValue = *min_element(v.begin(),v.end());
+* 普通数组
+最大值：int maxValue = *max_element(a,a+6); 
+最小值：int minValue = *min_element(a,a+6);
+2. 求数组最大值最小值对应的下标
+* vector容器
+最大值下标：int maxPosition = max_element(v.begin(),v.end()) - v.begin(); 
+最小值下标：int minPosition = min_element(v.begin(),v.end()) - v.begin();
+* 普通数组
+最大值下标：int maxPosition = max_element(a,a+6) - a; 
+最小值下标：int minPosition = min_element(a,a+6) - a;
+[参考](https://www.cnblogs.com/Tang-tangt/p/9352093.html)
